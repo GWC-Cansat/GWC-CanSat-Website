0b4f43c34c78443467aa7c15c908de1b1ad01fdb
Abhijith Ganesan <abhijithganesan@gmail.com>
2019-04-01 13:02:30 +0100

    Blog update and CSS optimisation

--- scripts/competition.js
+++ scripts/competition.js
@@ -1,4 +1,4 @@
-var countDownDate = new Date("Apr 4, 2019 15:00:00").getTime();
+var countDownDate = new Date("Apr 4, 2019 14:00:00").getTime();
 
 var x = setInterval(function() {
     var now = new Date().getTime();
